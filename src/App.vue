677c6f892368189d9dfe370b8f1f22ec55629ef8
Abir Choudhury <Abir.Choudhury@teamd3.com>
2019-05-10 12:37:57 -0500

    Second commit

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,47 @@
 <template>
-  <div id="app">    
+  <div id="app">
     <header>
       <nav>
         <ul>
           <li class="nav-item">
-            <img class="logo" src="./assets/build-a-bot-logo.png"/>
+            <img class="logo" src="./assets/build-a-bot-logo.png">
             Build-a-Bot
           </li>
         </ul>
       </nav>
     </header>
     <main>
-      <HomePage/>
+      <RobotBuilder/>
     </main>
   </div>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue';
+//import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'app',
   components: {
-    HomePage,
+    RobotBuilder,
   },
 };
 </script>
 
 <style>
-body{
+body {
   background: linear-gradient(to bottom, #555, #999);
   background-attachment: fixed;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
-main{
+main {
   margin: 0 auto;
   padding: 30px;
   background-color: white;
